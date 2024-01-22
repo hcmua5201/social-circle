968205209c30c2967f16d50f9f27d565ff39a34e
繁星_逐梦 <printlndarling@qq.com>
2024-01-22 19:06:32 +0800

    重新提交，发布界面的图片九宫格前端实现

--- social-circle-frontend/src/views/publish.vue
+++ social-circle-frontend/src/views/publish.vue
@@ -14,22 +14,24 @@
       <div id="picInput">
         <el-upload
             class="avatar-uploader"
+            list-type="picture"
             :http-request="upload"
             :show-file-list="false"
             :before-upload="beforeAvatarUpload"
             multiple
         >
           <img
-              v-if="images.length < 9"
-              class="avatar default-avatar"
-              src="../assets/insertimg.png"
-          />
-          <img
               v-for="(image, index) in images"
               :key="index"
               :src="image.url"
               class="avatar"
               @click="viewImage(image.url)"
+          />
+
+          <img
+              v-if="images.length < 9"
+              class="avatar default-avatar"
+              src="../assets/insertimg.png"
           />
         </el-upload>
       </div>
@@ -239,6 +241,8 @@ export default {
   outline: none;
   text-indent: 1em;
   min-height: 100px;
+  max-width: 100%;
+  min-width: 100%;
 }
 
 .container .bottom {
@@ -248,14 +252,16 @@ export default {
 .container .bottom #picInput {
   width: 100%;
   margin-top: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+}
+
+>>>.avatar-uploader .el-upload--picture {
+  display: block;
 }
 
 .container .bottom #picInput img {
-  width: calc(33.33% - 10px);
+  width: 30%;
   height: auto;
-  margin-bottom: 10px;
+  margin-top: 10px;
+  margin-left: 10px;
 }
 </style>
